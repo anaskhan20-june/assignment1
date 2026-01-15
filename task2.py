38c632b7e44764a5fb9af228c07cd179758d7822
Anas Khan <akhan50652@gmail.com>
2026-01-15 14:41:32 +0530

    Refactor greeting message to use f-string

--- task2.py
+++ task2.py
@@ -6,6 +6,6 @@ Creating a personalized Greeting
 first_name = input("Enter your First name: ")
 last_name = input("Enter your Last name: ")
 
-full_name = first_name + ' ' + last_name + '!'
+full_name = first_name +' '+ last_name
 
-print("Hello,", full_name, "welcome to the python program")
+print(f"Hello, {full_name}! welcome to the python program.")
